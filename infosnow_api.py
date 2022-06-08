c02fffb296aaf9938523117afebe34cb0bd50f0e
Jannick <jannick.schroer@gmail.com>
2022-06-08 17:24:21 +0200

    + Added Documentation + Changed from Class to Function

--- infosnow_api.py
+++ infosnow_api.py
@@ -13,93 +13,91 @@ def remove_html_tags(text: str):
     return text.strip()
 
 
-class API:
-    url = 'https://www.infosnow.ch/~apgmontagne/'
-    data = {}
-    html = ''
+def get_infosnow_data (resort_id, season):
+    """
+    Gets Data from the Infosnow website.
+    :param resort_id: ID of the resort you want to pull
+    :param season: Season of the data you want to pull 1: Winter, 2: Summer
+    :return: Dictionary filled with data on the stuff you requested.
+    """
+    rq = requests.get('https://www.infosnow.ch/~apgmontagne/',
+                      params={'tab': 'web-js', 'lang': 'de', 'xid': resort_id, 'saison': season},
+                      headers={'User-Agent': 'Totally Not a Bot'})
 
-    def __init__(self, resort_id, season):
-        self.resort_id = resort_id
-        self.season = season
-        self.load_data()
-        self.read_data()
+    if rq.status_code != 200:  # Return None if status code is not 200
+        print('Request didn\'t return status 200')
+        return None
 
-    def load_data(self):
-        rq = requests.get(self.url, params={'tab': 'web-js', 'lang': 'de', 'xid': self.resort_id, 'saison': self.season},
-                          headers={'User-Agent': 'Totally Not a Bot'})
+    html = rq.content.decode("utf-8")  # Decodes request to readable text.
+    data = {}  # Dictionary where information is stored
 
-        self.html = rq.content.decode("utf-8")
+    position = html.find('class="isH1"')  # Searches for Headers
+    while position > -1:
+        # Isolates header
+        position = html.find('>', position + 1)
+        position = html.find('>', position + 1)
+        end = html.find('</div>', position + 1)
 
-    def read_data(self):
-        position = self.html.find('class="isH1"')
-        while position > -1:
-            position = self.html.find('>', position + 1)
-            position = self.html.find('>', position + 1)
-            end = self.html.find('</div>', position + 1)
+        header = html[position + 1:end]
 
-            header = self.html[position + 1:end]
+        if 'offen)' in header:  # Checks that the headers contain important information
+            h_end = header.find(' (')
+            header = remove_html_tags(header[:h_end])
+        else:
+            position = html.find('class="isH1"', position)
+            continue
 
-            if 'offen)' in header:
-                h_end = header.find(' (')
-                header = remove_html_tags(header[:h_end])
+        entries = []  # List of Entries in the Header
+
+        next_entry = html.find('<div class="isRec">', position)
+
+        next_data = html.find('<div class="isBreakBox">', position)
+        if next_data < 0:
+            next_data = html.find('<img class="isAPG"', position)
+
+        print(f'Started Header: {header}')
+        while next_entry < next_data:  # Gets each entry and adds it's information to data
+            next_entry = html.find('~apgmontagne/pub/pics/state/3/', next_entry)
+            next_entry = html.find(' alt="', next_entry)
+            status = remove_html_tags(html[next_entry + 6:html.find('" title="', next_entry)])
+
+            next_entry = html.find('~apgmontagne/pub/pics/svg/', next_entry)
+            next_entry = html.find(' alt="', next_entry)
+            category = remove_html_tags(html[next_entry + 6:html.find('" title="', next_entry)])
+
+            next_entry = html.find('<div class="isTxt">', next_entry)
+            text_end = html.find('</div>', next_entry)
+            span = html.find('<span', next_entry)
+            if 0 < span < text_end:
+                name = remove_html_tags(html[next_entry + 19:span])
+                span = html.find('>', span)
+                span_end = html.find('</span>', span)
+                length = remove_html_tags(html[span + 2:span_end - 1])
             else:
-                position = self.html.find('class="isH1"', position)
-                continue
+                name = remove_html_tags(html[next_entry + 19:text_end])
+                length = None
 
-            entries = []
+            if ' &bull; ' in name:
+                point = name.find(' &bull; ')
+                label = name[:point]
+                name = name[point + 8:]
+            else:
+                label = None
 
-            next_entry = self.html.find('<div class="isRec">', position)
+            doc = {'name': name, 'status': status, 'type': category}
 
-            next_data = self.html.find('<div class="isBreakBox">', position)
-            if next_data < 0:
-                next_data = self.html.find('<img class="isAPG"', position)
+            if label is not None:
+                doc['label'] = label
 
-            print(f'Started Header: {header}')
-            while next_entry < next_data:
-                next_entry = self.html.find('~apgmontagne/pub/pics/state/3/', next_entry)
-                next_entry = self.html.find(' alt="', next_entry)
-                status = remove_html_tags(self.html[next_entry + 6:self.html.find('" title="', next_entry)])
+            if length is not None:
+                doc['length'] = length
+            entries.append(doc)
 
-                next_entry = self.html.find('~apgmontagne/pub/pics/svg/', next_entry)
-                next_entry = self.html.find(' alt="', next_entry)
-                category = remove_html_tags(self.html[next_entry + 6:self.html.find('" title="', next_entry)])
+            next_entry = html.find('<div class="isRec">', next_entry)
+            if next_entry < 0:
+                break
 
-                next_entry = self.html.find('<div class="isTxt">', next_entry)
-                text_end = self.html.find('</div>', next_entry)
-                span = self.html.find('<span', next_entry)
-                if 0 < span < text_end:
-                    name = remove_html_tags(self.html[next_entry + 19:span])
-                    span = self.html.find('>', span)
-                    span_end = self.html.find('</span>', span)
-                    length = remove_html_tags(self.html[span + 2:span_end - 1])
-                else:
-                    name = remove_html_tags(self.html[next_entry + 19:text_end])
-                    length = None
+        position = html.find('class="isH1"', position)
+        data[header] = entries
 
-                if ' &bull; ' in name:
-                    point = name.find(' &bull; ')
-                    label = name[:point]
-                    name = name[point + 8:]
-                else:
-                    label = None
-
-                doc = {'name': name, 'status': status, 'type': category}
-
-                if label is not None:
-                    doc['label'] = label
-
-                if length is not None:
-                    doc['length'] = length
-                entries.append(doc)
-
-                next_entry = self.html.find('<div class="isRec">', next_entry)
-                if next_entry < 0:
-                    break
-
-            position = self.html.find('class="isH1"', position)
-            self.data[header] = entries
-
-
-api = API(54, 1)
-print(api.data)
-    
+    return data  # Returns Data
